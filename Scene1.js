ea42d84bfbcd57e1ffbc8eb3e051640cf06795c8
yonah-trachtman <yonah.trachtman@gmail.com>
2024-05-02 14:28:54 +0300

    assets

--- Scene1.js
+++ Scene1.js
@@ -1,5 +1,12 @@
-class Scene1 extends Phaser.scene{
+class Scene1 extends Phaser.Scene{
     constructor() {
         super("bootGame");
     }
+    create() {
+        this.add.text(20, 20, "Loading game...");
+        this.scene.start("playGame");
+    }
 }
+
+
+
--- Scene2.js
+++ Scene2.js
@@ -1,5 +1,8 @@
-class Scene2 extends Phaser.scene{
+class Scene2 extends Phaser.Scene{
     constructor() {
         super("playGame");
+    }
+    create() {
+        this.add.text(20, 20, "playing game", {font: "25px Arial", fill: "yellow"})
     }
 }
